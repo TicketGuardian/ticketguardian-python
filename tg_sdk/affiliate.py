429a42f421679306be6d614902ddd53cff186d75
Justin Robles <J.Robles@ticketguardian.net>
2018-07-23 11:07:03 -0700

    Add TODO

--- tg_sdk/affiliate.py
+++ tg_sdk/affiliate.py
@@ -12,7 +12,13 @@ class Affiliate(ListResourceMixin, RetrieveResourceMixin,):
 
     @property
     def parent(self):
+        if self._parent is None:
+            return self.construct({})
         if not hasattr(self._parent, 'resource'):
+            # TODO(Justin): This should construct an object and not make an
+            #               API call. API calls should only be made when an
+            #               attr of the parent is called.
+
             self._parent = self.retrieve(self._parent.id)
         return self._parent
 
