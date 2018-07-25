f73a7a8d9dfd0af631ba327bd2d330591d7220e4
Justin Robles <J.Robles@ticketguardian.net>
2018-07-25 14:46:03 -0700

    Make customer inherit RetrieveResourceMixin

--- tg_sdk/customer.py
+++ tg_sdk/customer.py
@@ -1,7 +1,7 @@
-from tg_sdk.abstract.api_resource import APIResource
+from tg_sdk.abstract.retrieve_resource import RetrieveResourceMixin
 
 
-class Customer(APIResource,):
+class Customer(RetrieveResourceMixin,):
     # Todo(Justin): This is barebones for now just so I can have an object to
     #               return from other resource classes. I will add more
     #               functionality to this once I make more progress on those
