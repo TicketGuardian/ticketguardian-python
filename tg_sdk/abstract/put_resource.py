52f66414c5b6db1dc51ea01d1f2487ad91a867c4
Justin Robles <J.Robles@ticketguardian.net>
2018-08-02 15:59:09 -0700

    Add optional id as keyword argument

--- tg_sdk/abstract/put_resource.py
+++ tg_sdk/abstract/put_resource.py
@@ -11,13 +11,15 @@ class PutResourceMixin(APIResource):
             Keyword Arguments:
                 ext: An extension of the url if extra args are needed.
                      Does not need the leading or trailing '/'.
+                id: An optional resource id. If none is given then use the
+                    instances id. This should only be used by resource classes.
 
             Returns:
                 An instance of the object that is being updated.
                 If a bad request is made then an empty instance of the resource
                 object is returned.
         """
-        url = self.make_url(self.id)
+        url = self.make_url(params.pop('id', self.id))
 
         ext = params.pop('ext', None)
         if ext:
