f88ec7d937f8a5b5f4cdd074769c33d47a200aa8
Justin Robles <j.robles@ticketguardian.net>
2018-10-03 22:36:49 -0700

    Update test_exchange_policy

--- tg_sdk/policy/tests/test_exchange_policy.py
+++ tg_sdk/policy/tests/test_exchange_policy.py
@@ -1,8 +1,10 @@
 import tg_sdk
+from tg_sdk._project._decorators import client_test_method
 
 
+@client_test_method
 def test_exchange_policy():
-    policy = tg_sdk.Policy.list(limit=1)[0]
+    policy = tg_sdk.Policy.list(limit=1, status='Issued')[0]
     updated_before = policy.updated
     params = {
         "item": {
@@ -16,6 +18,7 @@ def test_exchange_policy():
     policy.exchange(**params)
 
     item = params.get('item')
+    item['cost'] = float(item['cost'])
     exchanged_item = policy.item
     updated_after = policy.updated
 
