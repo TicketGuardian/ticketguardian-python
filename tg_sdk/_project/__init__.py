81e6b5dd4a10c6b7814ae2866158d251aaa44141
Justin Robles <j.robles@ticketguardian.net>
2018-10-03 20:22:02 -0700

    Change .env path

--- tg_sdk/_project/__init__.py
+++ tg_sdk/_project/__init__.py
@@ -1,11 +1,13 @@
 import os
 
 DEFAULT_ENV_NAME = '.env'
-DEFAULT_ENV_PATH = "../../{}".format(DEFAULT_ENV_NAME)
+DEFAULT_ENV_PATH = "./{}".format(DEFAULT_ENV_NAME)
 
 if os.path.exists(DEFAULT_ENV_PATH):
     with open(DEFAULT_ENV_PATH) as f:
         for line in f:
+            if line == '\n' or line[0] == '#':
+                pass
             key, value = line.split('=')
             if os.environ.get(key) is None:
                 os.environ.setdefault(key, value)
