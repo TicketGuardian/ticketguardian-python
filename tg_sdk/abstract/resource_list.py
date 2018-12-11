1626482e608096278a30e4addb7f6d9fd0b83a4d
Justin Robles <j.robles@ticketguardian.net>
2018-12-11 14:59:18 -0800

    Update ResourceList to use defaultdict

--- tg_sdk/abstract/resource_list.py
+++ tg_sdk/abstract/resource_list.py
@@ -1,98 +1,135 @@
-from math import ceil
+from collections import defaultdict
+from math import floor
 
-PAGE_SIZE = 20
+from tg_sdk.constants import PAGE_SIZE
 
 
 class LazyLoadMixin:
     def _update_list(self, offset):
         """
         Used to lazy load self._data when an page that has not been loaded
-        is called. This will make an API request to get a list of objects
-        with the size PAGE_SIZE or size of remaining spots in list.
+        is called.
         """
-        limit = None
-        if offset + PAGE_SIZE > self._limit:
-            # If the index at the end of the page is larger than
-            # the size of the list then we need the number of spaces
-            # left to fill
-            limit = PAGE_SIZE - ((offset + PAGE_SIZE) - self._limit)
-
         obj_list = self._cls().get_list(
             offset=offset,
-            limit=limit,
             *self._ext,
             **self._params)
 
         start_ind = offset
-        end_ind = offset + PAGE_SIZE
+        end_ind = offset + len(obj_list)
 
-        if limit:
-            end_ind = self._limit
+        for i in range(start_ind, end_ind):
+            self._data[i] = obj_list[i - start_ind]
 
-        self._data[start_ind:end_ind] = obj_list
+    def _get_offset(self, ind):
+        page_number = floor(ind / (PAGE_SIZE))
+        return page_number * PAGE_SIZE
 
 
-class Resource_List(list, LazyLoadMixin):
-    def __init__(self, cls, limit=None, *ext, **params):
+class ResourceList(list, LazyLoadMixin):
+    def __init__(self, cls, size=None, data=None, slice_ind=0, *ext, **params):
+        """
+        cls: The class of the objects being listed
+        size: The size of the ResourceList
+              Defaults to count of resources and only used as a kwarg interally
+              on sliced lists.
+        data: A defaultdict containing the objects.
+              Only used as a kwarg for sliced lists.
+        slice_ind: Used to calculate the index of a sliced list since the same
+                   data is used.
+        ext: Strings that need to be added to the end of the URL.
+        params: Extra params for the api call.
+        """
         self._cls = cls
+        self._size = size or self._cls().get_resource_count()
+        self._data = data or defaultdict(lambda: None)
+        self._slice_ind = slice_ind
         self._ext = ext
         self._params = params
-        resource_count = self._cls().get_resource_count()
-        if limit is None or limit > resource_count:
-            self._limit = resource_count
-        else:
-            self._limit = limit
-        self._data = [None] * self._limit
 
     def __repr__(self):
-        return str(self._data)
+        return F"<{self._cls.__name__} ResourceList: {self._size} objects>"
 
     def __iter__(self):
-        return Resource_Iterator(
+        return ResourceIterator(
             cls=self._cls,
-            limit=self._limit,
+            size=self._size,
             data=self._data,
+            slice_ind=self._slice_ind,
             *self._ext,
             **self._params
         )
 
     def __getitem__(self, ind):
-        temp = ind
-        if self._data[ind] is None:
-            if ind == 0:
-                temp += 1
-            if ind < 0:
-                temp += self._limit
+        """
+        Returns a new instance of ResourceList if a sliced list is needed.
+        """
+        if isinstance(ind, slice):
+            start = ind.start or 0
+            stop = ind.stop or self._size
+            return ResourceList(
+                cls=self._cls,
+                size=stop - start,
+                data=self._data,
+                slice_ind=start,
+                *self._ext,
+                **self._params,
+            )
+        if ind >= self._size:
+            raise IndexError('list index out of range')
+        if ind < 0:
+            # If index negative then get positive index
+            ind += self._size
+        else:
+            # Add offsetting index
+            ind += self._slice_ind
 
-            # Use PAGE_SIZE - 1 since index starts at 0
-            multiple = ceil(temp / (PAGE_SIZE - 1)) - 1
-            offset = multiple * PAGE_SIZE
+        if self._data[ind] is None:
+            offset = self._get_offset(ind)
             self._update_list(offset)
 
         return self._data[ind]
 
     def __len__(self):
-        return len(self._data)
+        return self._size
 
 
-class Resource_Iterator(LazyLoadMixin):
-    def __init__(self, cls, limit, data, *ext, **params):
+class ResourceIterator(LazyLoadMixin):
+    def __init__(self, cls, size, data, slice_ind=0, *ext, **params):
+        """
+        cls: The class of the objects being listed
+        size: The size of the ResourceList
+              Defaults to count of resources and only used as a kwarg interally
+              on sliced lists.
+        data: A defaultdict containing the objects.
+              Only used as a kwarg for sliced lists.
+        offset: The first value on the current page.
+        ind: The current index of the iterator.
+        slice_ind: Used to calculate the index of a sliced list since the same
+                   data is used.
+        ext: Strings that need to be added to the end of the URL.
+        params: Extra params for the api call.
+        """
         self._cls = cls
+        self._size = size
+        self._data = data
+        self._ind = 0
+        self._slice_ind = slice_ind
         self._ext = ext
         self._params = params
-        self._limit = limit
-        self._data = data
-        self._offset = 0
-        self._index = 0
 
     def __next__(self):
-        if self._index < self._limit:
+        if self._ind < self._size:
             if self._data[self._index] is None:
-                self._update_list(self._offset)
-                self._offset += 20
+                offset = self._get_offset(self._index)
+                self._update_list(offset)
 
             obj = self._data[self._index]
-            self._index += 1
+            self._ind += 1
             return obj
         else:
             raise StopIteration
+
+    @property
+    def _index(self):
+        return self._ind + self._slice_ind
