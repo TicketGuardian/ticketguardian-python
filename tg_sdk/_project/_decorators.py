ca119eb9ae973e2272b26c01e0624b1be9410d74
Justin Robles <j.robles@ticketguardian.net>
2019-01-09 16:57:33 -0800

    Update testing decorators for buddy

--- tg_sdk/_project/_decorators.py
+++ tg_sdk/_project/_decorators.py
@@ -1,10 +1,15 @@
+import os
+
 import tg_sdk
 
 
 def affiliate_test_method(func):
     def wrapper():
-        tg_sdk.PUBLIC_KEY = tg_sdk.constants.AFF_PUB
-        tg_sdk.SECRET_KEY = tg_sdk.constants.AFF_SEC
+        # Reading from environ for buddy tests
+        pub = tg_sdk.constants.AFF_PUB or os.environ.get('AFF_PUB')
+        sec = tg_sdk.constants.AFF_SEC or os.environ.get('AFF_SEC')
+        tg_sdk.PUBLIC_KEY = pub
+        tg_sdk.SECRET_KEY = sec
         tg_sdk.ENV = 'dev'
         func()
     return wrapper
@@ -12,8 +17,11 @@ def affiliate_test_method(func):
 
 def client_test_method(func):
     def wrapper():
-        tg_sdk.PUBLIC_KEY = tg_sdk.constants.CLI_PUB
-        tg_sdk.SECRET_KEY = tg_sdk.constants.CLI_SEC
+        # Reading from environ for buddy tests
+        pub = tg_sdk.constants.CLI_PUB or os.environ.get('CLI_PUB')
+        sec = tg_sdk.constants.CLI_SEC or os.environ.get('CLI_SEC')
+        tg_sdk.PUBLIC_KEY = pub
+        tg_sdk.SECRET_KEY = sec
         tg_sdk.ENV = 'dev'
         func()
     return wrapper
