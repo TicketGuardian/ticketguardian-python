0dce898fdcffb134878275c5b65b6d348d922da3
Justin Robles <j.robles@ticketguardian.net>
2019-02-13 16:50:05 -0800

    Add method to get all parents of an Affiliate

--- ticketguardian/affiliate/affiliate.py
+++ ticketguardian/affiliate/affiliate.py
@@ -18,3 +18,18 @@ class Affiliate(
         if self._parent is not None and not hasattr(self._parent, 'resource'):
             self._parent = self._construct(obj=self._parent)
         return self._parent
+
+    def get_parent_scope(self):
+        """
+        Used to gets all parents within the scope of the Affiliate.
+        Returns:
+            list: a list of all parents of the Affiliate
+        """
+        result = []
+        affiliate = self.parent
+
+        while affiliate is not None:
+            result += [affiliate]
+            affiliate = affiliate.parent
+
+        return result
