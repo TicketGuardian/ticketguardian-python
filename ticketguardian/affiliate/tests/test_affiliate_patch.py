58d05dec263630a51b83e2765e15d064e47babde
Justin Robles <j.robles@ticketguardian.net>
2019-02-13 16:51:43 -0800

    Fix failing tests

--- ticketguardian/affiliate/tests/test_affiliate_patch.py
+++ ticketguardian/affiliate/tests/test_affiliate_patch.py
@@ -3,6 +3,10 @@ from ticketguardian.affiliate import Affiliate
 
 def test_affiliate_patch():
     aff = Affiliate.list(limit=1)[0]
-    patch_result = aff.patch(
-        aff.id, name='test_patch')
-    assert aff.name == 'test_patch'
+    aff_name = aff.name
+
+    aff.patch(aff.id, name=aff_name + 'test_patch')
+    assert aff.name == aff_name + 'test_patch'
+
+    # Change name back
+    aff.patch(aff.id, name=aff_name)
