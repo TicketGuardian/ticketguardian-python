bc77e18fecf60a58595f6293ffd77bdbcb9248ed
Justin Robles <j.robles@ticketguardian.net>
2019-03-05 11:34:27 -0800

    Update get_scope to get own scope

--- ticketguardian/auth/auth.py
+++ ticketguardian/auth/auth.py
@@ -7,22 +7,11 @@ class Auth(RetrieveResourceMixin):
 
     resource = 'auth'
 
-    @classmethod
-    def get_scope(cls, root_type='', root_id=''):
+    def get_scope(cls):
         """
-        Get scope of Client, Affiliate, or own scope.
-
-        Arguments:
-            root_type {str} -- The type of object the root is.
-                               Should be 'Client' or 'Affiliate'.
-            root_id {str} -- The resource id of the root.
+        Get own scope.
 
         Returns:
-            list -- A list containing all objects within scope including itself
+            list -- A list containing all objects within scope including self
         """
-        if root_type.lower() == 'client':
-            return Client.retrieve(root_id).scope
-        elif root_type.lower() == 'affiliate':
-            return Affiliate.retrieve(root_id).scope
-        else:
-            return cls.retrieve('scope', raw_data=True)
+        return cls.retrieve('scope', raw_data=True)
