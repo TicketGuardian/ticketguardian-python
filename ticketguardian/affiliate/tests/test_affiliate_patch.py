99fb9cef5a5558442e70f9e4d184bb63bb5b2e5f
Justin Robles <j.robles@ticketguardian.net>
2019-03-05 17:11:42 -0800

    Update test cases

--- ticketguardian/affiliate/tests/test_affiliate_patch.py
+++ ticketguardian/affiliate/tests/test_affiliate_patch.py
@@ -4,9 +4,7 @@ from ticketguardian.affiliate import Affiliate
 def test_affiliate_patch():
     aff = Affiliate.list(limit=1)[0]
     aff_name = aff.name
-
     aff.patch(aff.id, name=aff_name + 'test_patch')
     assert aff.name == aff_name + 'test_patch'
-
     # Change name back
     aff.patch(aff.id, name=aff_name)
