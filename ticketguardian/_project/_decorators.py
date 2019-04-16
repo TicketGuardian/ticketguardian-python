39cdcfae921b5d926a73eacedda691468f4b5bbf
Justin Robles <j.robles@ticketguardian.net>
2019-04-16 10:08:18 -0700

    Change ENV to ENVIRONMENT

--- ticketguardian/_project/_decorators.py
+++ ticketguardian/_project/_decorators.py
@@ -7,7 +7,7 @@ def affiliate_test_method(func):
     def wrapper():
         ticketguardian.PUBLIC_KEY = os.environ.get('AFF_PUB')
         ticketguardian.SECRET_KEY = os.environ.get('AFF_SEC')
-        ticketguardian.ENV = 'dev'
+        ticketguardian.ENVIRONMENT = 'dev'
         func()
     return wrapper
 
@@ -16,6 +16,6 @@ def client_test_method(func):
     def wrapper():
         ticketguardian.PUBLIC_KEY = os.environ.get('CLI_PUB')
         ticketguardian.SECRET_KEY = os.environ.get('CLI_SEC')
-        ticketguardian.ENV = 'dev'
+        ticketguardian.ENVIRONMENT = 'dev'
         func()
     return wrapper
