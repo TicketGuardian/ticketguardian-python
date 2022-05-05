f7746a690a5d586aa27b1c3f82561ee87aa69768
Lance Admin <lanceadmin@LanceLeeM1MBP.attlocal.net>
2022-05-05 10:23:26 -0700

    add local configs

--- ticketguardian/constants.py
+++ ticketguardian/constants.py
@@ -4,11 +4,16 @@ BILLING_DEV = "https://billing-dev.ticketguardian.net"
 BILLING_PROD = "https://billing.ticketguardian.net"
 BILLING_SANDBOX = "https://billing-sandbox.ticketguardian.net"
 CORE_DEV = "https://connect.ticketguardian-dev.net"
+CORE_LOCAL = "docker.for.mac.host.internal:3000"
 CORE_PROD = 'https://connect.ticketguardian.net'
 CORE_SANDBOX = 'https://connect-sandbox.ticketguardian.net'
 CORE_QA = 'https://connect.ticketguardian-qa.com'
 
 DOMAINS = {
+    "local": {
+        "core": CORE_LOCAL,
+        "billing": BILLING_DEV
+    },
     "dev": {
         "core": CORE_DEV,
         "billing": BILLING_DEV
